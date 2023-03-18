c174046b4c8f3114c3df619fb371b69295b79825
Laura <LNKEAT@gmail.com>
2023-03-18 16:49:37 -0600

    missing error message

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,33 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+
+
+  def index
+    authors = Author.all
+    render json: authors
+  end
+
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
     author = Author.create(author_params)
-
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def render_not_found_response
+    render json: { error: "Author not found" }, status: :not_found
   end
   
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def index
+    posts = Post.all 
+    render json: posts
+  end
+
+  def create
+    post = Post.create(post_params)
+    render json: post
+  end
 
   def show
     post = Post.find(params[:id])
